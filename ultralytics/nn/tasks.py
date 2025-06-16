c330e6918df5e7440a17a931f05de00daa832c22
Dzhyyy <3491290994@qq.com>
2025-06-16 16:21:31 +0800

    improve commit

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -67,7 +67,8 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
-    SE_Block
+    SE_Block,
+    MobileNetV4ConvSmall
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -1732,8 +1733,19 @@ def parse_model(d, ch, verbose=True):
             c2 = args[0]
             c1 = ch[f]
             args = [*args[1:]]
+        elif m in {MobileNetV4ConvSmall, }:
+            c2 = args[1]
+            c1 = args[2]
         else:
             c2 = ch[f]
+
+        if m in {MobileNetV4ConvSmall, }:
+            m_ = m(args[2])  # module
+            t = "ultralytics.nn.modules.mobilenetv4"
+        else:
+            m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
+            t = str(m)[8:-2].replace("__main__.", "")  # module type
+
 
         m_ = torch.nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
         t = str(m)[8:-2].replace("__main__.", "")  # module type
